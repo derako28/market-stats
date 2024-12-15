5e7805208d9f696b0147e43354f79d23c0bb41c8
Serhii Talala <Talala.SH@uds.ua.energy>
2024-12-15 17:12:00 +0100

    changed styles

--- src/components/share/Header/header.jsx
+++ src/components/share/Header/header.jsx
@@ -1,18 +1,20 @@
-import {Link} from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export const Header = () => {
-    return (
-        <div className={'flex gap-8 text-gray-300 p-4 mb-8'}>
-            {/*<Link to={'/market-stats/stats-table'} >Table</Link>*/}
-            {/*<Link to={'/market-stats/stats-table-new'} >Table New</Link>*/}
-            {/*<Link to={'/market-stats/stats-charts'} >Charts 2023</Link>*/}
-            {/*<Link to={'/market-stats/stats-charts-2024'} >Charts 2024</Link>*/}
-            <Link to={'/market-stats/stats-charts-es'} >Charts ES</Link>
-            <Link to={'/market-stats/stats-charts-nq'} >Charts NQ</Link>
-            {/*<Link to={'/market-stats/backtests'} >Backtests</Link>*/}
-            {/*<Link to={'/market-stats/backtests-sanya'} >Backtests Sanya</Link>*/}
-            {/*<Link to={'/market-stats/backtests-tapok'} >Backtests Tapok</Link>*/}
-            {/*<Link to={'/market-stats/backtests-tapok-US500'} >Backtests Tapok US500</Link>*/}
-        </div>
-    )
-}
+  return (
+    <div className={"flex gap-8 text-gray-300 p-4 mb-8"}>
+      {/*<NavLink to={"/market-stats/stats-table"}>Table</NavLink>*/}
+      {/*<NavLink to={"/market-stats/stats-table-new"}>Table New</NavLink>*/}
+      {/*<NavLink to={"/market-stats/stats-charts"}>Charts 2023</NavLink>*/}
+      {/*<NavLink to={"/market-stats/stats-charts-2024"}>Charts 2024</NavLink>*/}
+      <NavLink to={"/market-stats/stats-charts-es"}>Charts ES</NavLink>
+      <NavLink to={"/market-stats/stats-charts-nq"}>Charts NQ</NavLink>
+      {/*<NavLink to={"/market-stats/backtests"}>Backtests</NavLink>*/}
+      {/*<NavLink to={"/market-stats/backtests-sanya"}>Backtests Sanya</NavLink>*/}
+      {/*<NavLink to={"/market-stats/backtests-tapok"}>Backtests Tapok</NavLink>*/}
+      {/*<NavLink to={"/market-stats/backtests-tapok-US500"}>*/}
+      {/*  Backtests Tapok US500*/}
+      {/*</NavLink>*/}
+    </div>
+  );
+};
