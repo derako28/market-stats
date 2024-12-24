024eef85fb106cecbe970e3d983604b20be6818b
Serhii Talala <Talala.SH@uds.ua.energy>
2024-12-24 13:44:17 +0100

    refactoring

--- src/pages/ES/Statistic/Statistic.jsx
+++
@@ -1,8 +0,0 @@
-import css from "./statistic.module.scss";
-export const Statistic = ({ data }) => {
-  return (
-    <div className={"px-4 flex my-8"}>
-      {<div className={css.root}>Days: {data.length}</div>}
-    </div>
-  );
-};
